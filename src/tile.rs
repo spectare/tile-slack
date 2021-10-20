c734028e20a9a298c0e91a26fb67123a4a8ebd85
Olger Warnier <olger@spectare.nl>
2021-10-20 16:21:23 +0200

    update on the tile size and font

--- src/tile.rs
+++ src/tile.rs
@@ -9,33 +9,33 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum TileError {
-    ///It was not possible to laod the keys used for validation of the signature
-    #[error("Failed to load JWKS keystore from {0:?}")]
-    FailedToLoadKeystore(reqwest::Error),
+  ///It was not possible to laod the keys used for validation of the signature
+  #[error("Failed to load JWKS keystore from {0:?}")]
+  GenericTileError(String),
 }
 
 pub async fn create_tile_image(text: String) -> Result<web::Bytes, TileError> {
-    let font_data: &[u8] = include_bytes!("../resources/alex_brush/AlexBrush-Regular.ttf");
-    let font: Font<'static> = Font::try_from_bytes(font_data)
-        .expect("Map bytes to Font failed for AlexBrush-Regular.ttf");
+  let font_data: &[u8] =
+    include_bytes!("../resources/Dancing_Script/static/DancingScript-Bold.ttf");
+  let font: Font<'static> = Font::try_from_bytes(font_data).expect("Map bytes to Font failed");
 
-    let annotation = Annotation {
-        position: Position::Middle,
-        text: text,
-    };
+  let annotation = Annotation {
+    position: Position::Middle,
+    text: text,
+  };
 
-    let image_data: &[u8] = include_bytes!("../resources/tegeltje.jpg");
-    let mut canvas = Canvas::read_from_buffer(image_data)
-        .expect("create Canvas from bytes for tegeltje.jpg failed");
-    canvas.add_annotation(&annotation, &font, 1.0);
-    canvas.render();
+  let image_data: &[u8] = include_bytes!("../resources/tegeltje_small.jpg");
+  let mut canvas =
+    Canvas::read_from_buffer(image_data).expect("create Canvas from bytes for tegeltje.jpg failed");
+  canvas.add_annotation(&annotation, &font, 1.0);
+  canvas.render();
 
-    let mut buffer = vec![].writer();
-    buffer.get_mut().reserve(604800);
+  let mut buffer = vec![].writer();
+  buffer.get_mut().reserve(604800);
 
-    canvas
-        .save_png(&mut buffer)
-        .expect("Tried to store the image canvas in the buffer");
-    let web_bytes = web::Bytes::copy_from_slice(buffer.get_ref());
-    Ok(web_bytes)
+  canvas
+    .save_png(&mut buffer)
+    .expect("Tried to store the image canvas in the buffer");
+  let web_bytes = web::Bytes::copy_from_slice(buffer.get_ref());
+  Ok(web_bytes)
 }
